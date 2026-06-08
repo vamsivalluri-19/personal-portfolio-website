4675f0adc062e45dc5534837051d48dd84be05a8
Vamsi Valluri <your-email@example.com>
2026-06-08 09:45:54 +0530

    Fix Render deployment

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,32 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// Middleware
 app.use(express.json());
 
-const path = require("path");
+app.use(
+  cors({
+    origin: [
+      "https://personal-portfolio-website-blue-theta.vercel.app/",
+      process.env.FRONTEND_URL
+    ],
+    credentials: true
+  })
+);
 
+// Routes
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/contact", require("./routes/contactRoutes"));
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Portfolio Backend Running"
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
